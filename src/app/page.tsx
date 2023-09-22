bb49da8a6fe4970199f966c5a31b01050b2932b2
kenrickpd <kenrickpd@gmail.com>
2023-09-22 17:38:43 +0700

    Added project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Introduction from "@/components/introduction";
-import SectionDivider from "@/components/section-divider";
+import Introduction from '@/components/introduction';
+import Projects from '@/components/projects';
+import SectionDivider from '@/components/section-divider';
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center px-4 h-[5000px]">
-      <Introduction />
-      <SectionDivider />
-    </main>
-  )
+    return (
+        <main className='flex flex-col items-center px-4 h-[5000px]'>
+            <Introduction />
+            <SectionDivider />
+            <Projects />
+        </main>
+    );
 }
