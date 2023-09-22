f0faddc647c0b71917613aae0ecea2d09f3d5c62
kenrickpd <kenrickpd@gmail.com>
2023-09-22 17:38:31 +0700

    Added data for project section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,18 +2,11 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/asset/corpcomment.png";
-import rmtdevImg from "@/asset/rmtdev.png";
-import wordanalyticsImg from "@/asset/wordanalytics.png";
 
 export const links = [
   {
     name: "Home",
     hash: "#home",
-  },
-  {
-    name: "About",
-    hash: "#about",
   },
   {
     name: "Projects",
@@ -32,3 +25,38 @@ export const links = [
     hash: "#contact",
   },
 ] as const;
+
+export const projectsData = [
+  {
+    title: "Election BNCC", 
+    description: "Election BNCC is an election website to vote for the next CEO on my organization called BNCC. My role here is as back-end developer.",
+    tags: ["Next.js","Express.js", "PostgreSQL", "Routing-controllers", "Typeorm"],
+    imageUrl: "/electionbncc.png",
+    width: '1898',
+    height: '1079',
+  }, 
+  {
+    title: "Foodtura", 
+    description: "Foodtura is a platform that will be focused on a restaurant management. My role here is as back-end developer",
+    tags: ["Next.js","Express.js", "PostgreSQL", "Routing-controllers", "Typeorm"],
+    imageUrl: "/foodtura.png",
+    width: '1010',
+    height: '715',
+  },
+  {
+    title: "blogskuy", 
+    description: "Blogskuy is a blog website. This project isn’t completely done but my job as a back-end web developer is already done.",
+    tags: ["Next.js","Express.js", "MySQL", "Routing-controllers", "Typeorm"],
+    imageUrl: "/blogskuy.png",
+    width: '1370',
+    height: '715',
+  },
+  {
+    title: "Himtikit Website", 
+    description: "This website is a learning kit website for the students on my university. This is my first project using a framework instead of just using vanilla HTML, CSS and JS.",
+    tags: ["Laravel", "PHP", "Blade"],
+    imageUrl: "/Himtikit.png",
+    width: '1370',
+    height: '776',
+  }
+]as const;
