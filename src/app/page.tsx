31195acd0ca6ff4fd5a7d2858b188eab54800361
kenrickpd <kenrickpd@gmail.com>
2023-09-22 14:38:47 +0700

    Added section divider with animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Introduction from "@/components/introduction";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4 h-[5000px]">
       <Introduction />
+      <SectionDivider />
     </main>
   )
 }
