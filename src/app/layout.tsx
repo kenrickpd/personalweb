739e0cda8807c2a5fd62ee64de91b029aa0a7d2f
kenrickpd <kenrickpd@gmail.com>
2023-09-23 12:09:02 +0700

    Configured favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,29 @@ export default function RootLayout({
 }) {
     return (
         <html lang='en' className='!scroll-smooth'>
+            <head>
+                <link rel='shortcut icon' href='/images/favicon.ico' />
+                <link
+                    rel='apple-touch-icon'
+                    sizes='180x180'
+                    href='/images/apple-touch-icon.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='/images/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='/images/favicon-16x16.png'
+                />
+            </head>
             <body
                 className={
-                    '${inter.className} bg-purple-200 text-gray-950 relative pt-28 sm:pt-36'
+                    '${inter.className} bg-violet-200 text-gray-950 relative pt-28 sm:pt-36'
                 }
             >
                 <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.7rem]'></div>
