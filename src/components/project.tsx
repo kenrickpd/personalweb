7d913287dae6fadcf57b25a897a3df1408c30db0
kenrickpd <kenrickpd@gmail.com>
2023-09-23 18:30:03 +0700

    Hide project img in mobile

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -32,8 +32,8 @@ export default function Project({
             }}
             className='group mb-3 sm:mb-8 last:mb-0'
         >
-            <section className=' bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 rounded-lg'>
-                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
+            <section className=' bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 rounded-lg'>
+                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
                     <h3 className='text-2xl font-semibold'>{title}</h3>
                     <p className='mt-2 leading-relaxed text-gray-700'>
                         {description}
@@ -55,7 +55,7 @@ export default function Project({
                     quality={95}
                     width={width}
                     height={height}
-                    className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+                    className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
                 transition
                 group-hover:-translate-x-3
                 group-hover-scale-[1.04]
